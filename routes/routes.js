21ee6221212beffa5d6aa18a27bb9a91f9693104
Bliss Perry <blissperry123@gmail.com>
2018-01-19 10:19:05 -0500

    Bliss 10:19

--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const auth = require('../controllers/auth')
 const users = require('../controllers/users')
 const yelp = require('../controllers/yelp')
+const appointments = require('../controllers/appointments')
 
 
 /*
@@ -13,7 +14,7 @@ const yelp = require('../controllers/yelp')
 */
 router.route('/users')
   .post(users.createUser)
-  .get(auth.validateUser, users.getUserById)
+  .get(users.getUserById)
   .put(users.updateUser)
   .delete(auth.validateUser, users.deleteUser)
 
@@ -35,6 +36,14 @@ router.route('/auth/login')
 
 router.route('/yelpinfo')
   .get(yelp.getYelp)
+
+ /*
+ * Appointment Routes
+ */
+
+ router.route('/restaurant')
+ 	.post(appointments.createApt)
+ 	.get(appointments.getApts)
 
 // expose routes through router object
 module.exports = router;
